5b10bf97690a83d47797945f47e81b9088896dc8
hBISPO98 <b.hiandra@aluno.ifsp.edu.br>
2026-05-18 23:25:14 -0300

    Update ATT08.sql

--- ATT08.sql
+++ ATT08.sql
@@ -2,15 +2,15 @@
 -- Construa uma Trigger chamada dbo.lost_credits que atualiza o valor de créditos de um aluno após a retirada de um curso da sua lista.
 
 CREATE TRIGGER dbo.lost_credits
-ON dbo.takes       -- tabela vigiada
-AFTER DELETE       -- dispara quando o aluno retirar o curso da lista takes
+ON dbo.takes  -- tabela vigiada
+AFTER DELETE  -- dispara quando o aluno retirar o curso da lista takes
 AS
 BEGIN
-    SET NOCOUNT ON; -- elimina mensagens de linhas afetadas
+    SET NOCOUNT ON;  -- elimina mensagens de linhas afetadas
 
-    UPDATE s -- atualiza tabela students (s)
-    SET s.tot_cred = s.tot_cred - c.credits -- valor atual - créditos do curso
-    FROM dbo.student s -- altera os dados
-    INNER JOIN deleted d ON s.ID = d.ID -- cruza os dados do aluno deletado (d) com o ID de students (s) pra verificar se o ID do aluno é homogêneo
-    INNER JOIN dbo.course c ON d.course_id = c.course_id; -- '' o código da tabela course (c) com o c que foi deletado = descobre quantos créditos c valia.
+    UPDATE s  -- atualiza tabela students (s)
+    SET s.tot_cred = s.tot_cred - c.credits  -- valor atual - créditos do curso
+    FROM dbo.student s  -- altera os dados
+    INNER JOIN deleted d ON s.ID = d.ID  -- cruza os dados do aluno deletado (d) com o ID de students (s) pra verificar se o ID do aluno é homogêneo
+    INNER JOIN dbo.course c ON d.course_id = c.course_id;  -- '' o código da tabela course (c) com o c que foi deletado = descobre quantos créditos c valia.
 END;
